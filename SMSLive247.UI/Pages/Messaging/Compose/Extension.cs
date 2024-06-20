2dba68703bd179f2f2b433a6e9f985f45a8e62a9
omomowo symeon <omomowosymeon45@gmail.com>
2024-06-20 11:01:20 +0100

    message counter feature added

---
+++ SMSLive247.UI/Pages/Messaging/Compose/Extension.cs
@@ -0,0 +1,39 @@
+﻿namespace SMSLive247.UI.Pages.Messaging.Compose
+{
+    public static class Extension
+    {
+        public static int GetValidGsmTextLength(this string smsText)
+        {
+            smsText = smsText.Replace("\r", "");
+
+            var strGSMTable = "";
+            strGSMTable += "@£$¥èéùìòÇØøÅåΔ_ΦΓΛΩΠΨΣΘΞ`ÆæßÉ !\"#¤%&'()*=,-./0123456789:;<=>?¡";
+            strGSMTable += "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ`¿abcdefghijklmnopqrstuvwxyzäöñüà\r\n";
+
+            var strExtendedTable = "^{}\\[~]|€";
+            var cntGSMOutput = 0;
+
+            for (var i = 0; i < smsText.Length; i++)
+            {
+                var cPlainText = smsText[i];
+
+                var intGSMTable = strGSMTable.IndexOf(cPlainText);
+                if (intGSMTable != -1)
+                {
+                    cntGSMOutput += 1;
+                    continue;
+                }
+                var intExtendedTable = strExtendedTable.IndexOf(cPlainText);
+                if (intExtendedTable != -1)
+                {
+                    cntGSMOutput += 2;
+                }
+                else
+                {
+                    cntGSMOutput += 8;
+                }
+            }
+            return cntGSMOutput;
+        }
+    }
+}
