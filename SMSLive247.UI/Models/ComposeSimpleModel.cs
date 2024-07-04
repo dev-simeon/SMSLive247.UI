faa4af2d02dd2d6d13bdceac8ae5ffd5b483a3c4
Ajala Ayodeji <dejisys@idevworks.com>
2024-07-04 13:33:33 +0100

    sync1

---
+++ SMSLive247.UI/Models/ComposeSimpleModel.cs
@@ -0,0 +1,24 @@
+﻿using SMSLive247.OpenApi;
+
+namespace SMSLive247.UI.Pages.ViewModels
+{
+    public class ComposeSimpleModel
+    {
+        public List<string>? SenderIds { get; private set; }  
+        public SmsBatchRequest Request { get; private set; } = new();
+
+        public List<ContactModel> Contacts { get; private set; } = [];
+        public List<ContactModel> BatchFiles { get; private set; } = [];
+        public List<ContactModel> Numbers { get; set; } = [];
+
+        public void Initialize(
+            IEnumerable<SenderIdResponse> senderIds,
+            IEnumerable<ContactResponse> contacts,
+            IEnumerable<BatchFileResponse> batchFiles)
+        {
+            SenderIds = senderIds.Select(x => x.SenderID).ToList();
+            Contacts = contacts.Select(c => new ContactModel(c)).ToList();
+            BatchFiles = batchFiles.Select(b => new ContactModel(b)).ToList();
+        }
+    }
+}
---
+++ SMSLive247.UI/Models/GroupModel.cs
@@ -0,0 +1,24 @@
+﻿using SMSLive247.OpenApi;
+
+namespace SMSLive247.UI.Pages.ViewModels
+{
+    public record class GroupModel
+    {
+        public string Key { get; init; }
+        public string Name { get; init; }
+        public int Count { get; init; }
+        public bool Selected { get; set; } = false;
+        public bool Visible { get; set; } = true;
+        public GroupResponse Response { get; init; }
+        public IEnumerable<ContactModel> Contacts { get; set; } = [];
+
+        public GroupModel(GroupResponse g)
+        {
+            Key = g.GroupName;
+            Name = g.GroupName;
+            Count = g.Members.Count;
+            Contacts = g.Members.Select(x => new ContactModel(x));
+            Response = g;
+        }
+    }
+}
