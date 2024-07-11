a91251d9da4860c9f7046060d12b1fd12871fb25
omomowo symeon <omomowosymeon45@gmail.com>
2024-07-11 15:19:52 +0100

    update

--- SMSLive247.ApiClient/Connected Services/SmsLive Api Service/SmsApiClient.cs
+++ SMSLive247.ApiClient/Connected Services/SmsLive Api Service/SmsApiClient.cs
@@ -21,14 +21,15 @@ namespace SMSLive247.OpenApi
     using System = global::System;
 
     [System.CodeDom.Compiler.GeneratedCode("NSwag", "13.19.0.0 (NJsonSchema v10.9.0.0 (Newtonsoft.Json v13.0.3.0))")]
-    public partial class ApiClient 
+    public partial class ApiClient
     {
-        private string _baseUrl = "https://api.smslive247.com";
+        private string _baseUrl = "";
         private System.Net.Http.HttpClient _httpClient;
         private System.Lazy<System.Text.Json.JsonSerializerOptions> _settings;
 
-        public ApiClient(System.Net.Http.HttpClient httpClient)
+        public ApiClient(string baseUrl, System.Net.Http.HttpClient httpClient)
         {
+            BaseUrl = baseUrl;
             _httpClient = httpClient;
             _settings = new System.Lazy<System.Text.Json.JsonSerializerOptions>(CreateSerializerSettings);
         }
