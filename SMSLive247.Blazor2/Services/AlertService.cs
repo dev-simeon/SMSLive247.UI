e8f2edfdc75351a79fa0a5bf24ad144509764b45
Ajala Ayodeji <dejisys@idevworks.com>
2026-08-07 13:28:27 +0100

    Fixed AlertService

--- SMSLive247.Blazor2/Services/AlertService.cs
+++ SMSLive247.Blazor2/Services/AlertService.cs
@@ -7,25 +7,25 @@
         public event Func<string, string, Task>? OnSuccess;
         public event Func<string, string, Task<bool>>? OnConfirm;
 
-        public async Task Info(string message, string title)
+        public async Task Info(string message, string title = "Information")
         {
             if (OnInfo is not null)
                 await OnInfo.Invoke(message, title);
         }
 
-        public async Task Error(string message, string title)
+        public async Task Error(string message, string title = "Error")
         {
             if (OnError is not null)
                 await OnError.Invoke(message, title);
         }
 
-        public async Task Success(string message, string title)
+        public async Task Success(string message, string title = "Success")
         {
             if (OnSuccess is not null)
                 await OnSuccess.Invoke(message, title);
         }
 
-        public async Task<bool> Confirm(string message, string title)
+        public async Task<bool> Confirm(string message, string title = "Confirm")
         {
             if (OnConfirm is not null)
                 return await OnConfirm.Invoke(message, title);
