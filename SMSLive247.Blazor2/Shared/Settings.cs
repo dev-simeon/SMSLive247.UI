04c5a0cbc7ca920cd756ba88791b2212894b7cdf
Ajala Ayodeji <dejisys@idevworks.com>
2026-08-11 09:17:57 +0100

    sync

--- SMSLive247.Blazor2/Shared/Settings.cs
+++ SMSLive247.Blazor2/Shared/Settings.cs
@@ -3,7 +3,7 @@
     public class Settings
     {
         public string BaseUrl { get; set; } = "";
-        public string TawkToPropertyId { get; set; } = "";
+        public string TawkToID { get; set; } = "";
         public string ParentID { get; set; } = "a044b5a7-1147-45cb-871a-76546896c4f3";
         public int SmsMaxParts { get; set; } = 6; // Max SMS part
         public int MaxCharacter { get => SmsMaxParts * 153; } // Max SMS character
