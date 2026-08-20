2b182c0150ac53e4f8e8a707a18f845fdfc8ae1e
Ajala Ayodeji <dejisys@idevworks.com>
2026-08-20 14:21:40 +0100

    sync compose improvements

---
+++ SMSLive247.Blazor2/ViewModels/ComposeSimpleModel.cs
@@ -0,0 +1,300 @@
+﻿using CsvHelper;
+using System.Data;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using SMSLive247.UI.Services;
+using SMSLive247.OpenApi;
+
+namespace SMSLive247.Blazor2.Pages.ViewModels
+{
+    public abstract class BaseComposeModel
+    {
+        public string CounterText { get; set; } = "Type your Message here";
+        public List<SenderIdResponse> SenderIds { get; protected set; } = [];
+        public abstract long GrandTotal();
+    }
+
+    public class ComposeSimpleModel : BaseComposeModel
+    {
+        public SmsBatchRequest Request { get; private set; } = new();
+
+        public List<ContactModel> Contacts { get; private set; } = [];
+        public List<ContactModel> BatchFiles { get; private set; } = [];
+        public List<ContactModel> Numbers { get; private set; } = [];
+
+        public void UpdateContacts(List<ContactModel> items) => Contacts = items;
+        public void UpdateBulkfiles(List<ContactModel> items) =>  BatchFiles = items;
+        public void UpdateNumbers(List<ContactModel> items) => Numbers = items;
+
+        public override long GrandTotal()
+        {
+            int c1 = Contacts.Where(x => x.Selected).Sum(x => x.Count);
+            int c2 = Numbers.Where(x => x.Selected).Sum(x => x.Count);
+            int c3 = BatchFiles.Where(x => x.Selected).Sum(x => x.Count);
+            return c1 + c2 + c3;
+        }
+
+        public ComposeSimpleModel(
+                IEnumerable<SenderIdResponse> senderIds,
+                IEnumerable<ContactResponse> contacts,
+                IEnumerable<BatchFileResponse> batchFiles)
+        {
+            SenderIds = senderIds.ToList();
+            Contacts = contacts.Select(x => new ContactModel(x)).ToList();
+
+            BatchFiles = batchFiles
+                //.Where(x => x.FileType.ToLower() != "csv")
+               .Where(x => !string.Equals(x.FileType, "csv", StringComparison.OrdinalIgnoreCase))
+               .Select(x => new ContactModel(x))
+               .ToList();
+
+            Request.DeliveryTime = DateTime.Now;
+        }
+    }
+
+    public class ComposeTemplateModel : BaseComposeModel
+    {
+        public string DeliveryEmail = string.Empty; //TODO; add to API model
+        public SmsBatchCsvRequest Request { get; private set; } = new();
+
+
+        public List<BatchFileResponse> BatchCsvFiles { get; private set; } = [];
+        public List<DataColumn> DataColumns => dataTable.Columns.Cast<DataColumn>().ToList();
+        public List<DataRow> DataRows => dataTable.Rows.Cast<DataRow>().ToList();
+        public bool IsValidPhoneColumn => dataTable.IsPhoneNumberColumn(Request.PhoneNumberColumn);
+        //public bool IsLoaded => dataTable.Columns.Count > 0;
+
+        private readonly DataTable dataTable = new();
+
+        public ComposeTemplateModel(
+            IEnumerable<SenderIdResponse> senderIds,
+            IEnumerable<BatchFileResponse> batchFiles)
+        {
+            SenderIds = senderIds.ToList();
+            BatchCsvFiles = batchFiles.Where(x => x.FileType == "csv")
+                                      .OrderByDescending(x => x.DateCreated)
+                                      .ToList();
+            Request.DeliveryTime = DateTime.Now;
+        }
+
+        public void LoadData(Stream stream, string batchId)
+        {
+            using var reader = new StreamReader(stream);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            using var dr = new CsvDataReader(csv);
+
+            dataTable.Columns.Clear(); // ??
+            dataTable.Load(dr);
+            // Detect the column index for phone numbers
+            Request.PhoneNumberColumn = dataTable.DetectPhoneNumberColumn();
+            Request.BatchFileID = batchId;
+        }
+
+        public void ClearData()
+        {
+            dataTable.Clear();
+        }
+
+        public void ClearMessage()
+        {
+            Request.MessageText = string.Empty;
+            CounterText = "Type your Message here";
+        }
+
+        public override long GrandTotal()
+        {
+            //int c1 = BatchCsvFiles.Where(x => x.Selected).Sum(x => x.Count);
+            return BatchCsvFiles.Count;
+        }
+    }
+
+    public static class ComposeExtensions
+    {
+        public static List<T> DeepClone<T>(IEnumerable<T> source)
+        {
+            var json = JsonSerializer.Serialize(source);
+            return JsonSerializer.Deserialize<List<T>>(json)!;
+        }
+
+        //public static List<ContactModel> DeepClone(IEnumerable<ContactModel> source)
+        //{
+        //    var json = JsonSerializer.Serialize(source);
+        //    return  JsonSerializer.Deserialize<List<ContactModel>>(json)!;
+        //}
+
+        public static bool IsPhoneNumberColumn(this DataTable records, int columnIndex)
+        {
+            return records.Rows.OfType<DataRow>().ToList().All(row => IsPhoneNumber(row[columnIndex].ToString()));
+        }
+
+        public static int DetectPhoneNumberColumn(this DataTable records)
+        {
+            var columns = records.Columns.Cast<DataColumn>().ToList();
+
+            foreach (DataColumn column in columns)
+            {
+                var isValid = records.IsPhoneNumberColumn(column.Ordinal);
+                if (isValid) return column.Ordinal;
+            }
+            return -1; // Return -1 if no phone number column is detected
+        }
+
+        public static bool IsPhoneNumber(this string? value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            // Trim spaces from the value
+            value = value.Trim();
+
+            // Check if the value matches a common phone number pattern
+            // Length check: assuming phone numbers are between 7 and 15 digits long
+            // This regex allows for optional country code with +, spaces, dashes, and parentheses
+            var phonePattern = @"^(\+?\d{1,4}[-.\s]?)?(\(?\d{1,4}\)?[-.\s]?)?(\d{7,15})$";
+
+            return Regex.IsMatch(value, phonePattern);
+        }
+
+        public static string RemoveSpacesBetweenBraces(this string? input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return string.Empty;
+
+            var sourceParts = input.Split(['{', '}']);
+            var isEvenPositions = !input.StartsWith("{");
+
+            for (var p = 1; p <= sourceParts.Length; p++)
+            {
+                if (p % 2 == 0 & isEvenPositions)
+                    sourceParts[p - 1] = "{" + sourceParts[p - 1].Replace(" ", "").ToUpper() + "}";
+            }
+            return string.Join("", sourceParts);
+        }
+
+        public static async Task<string> CountSmsMessages(AlertService alert, string? strSmsText, int smsMaxParts)
+        {
+            if (string.IsNullOrWhiteSpace(strSmsText))
+            {
+                return "Type your Message here";
+            }
+
+            int intSmsLength = strSmsText.GetValidGsmTextLength();
+            int intSmsParts = GetMessageParts(intSmsLength);
+            int intNextMax = intSmsParts == 1 ? 160 : intSmsParts * 153;
+
+            if (intSmsParts > smsMaxParts)
+            {
+                await alert.Info("Maximum SMS characters reached!", "alert");
+                return string.Empty;
+            }
+
+            if (intSmsParts > 1)
+            {
+                if (intSmsLength == 161)
+                    await alert.Info($"You have just exceeded 160 characters. You will be charged {intSmsParts} pages for this message!", "alert");
+
+                if (intSmsLength == intNextMax - 153 + 1)
+                    await alert.Info($"You have just exceeded {intNextMax - 153} characters. You will be charged {intSmsParts} pages for this message!", "alert");
+            }
+            return $"{intSmsLength} / {intNextMax} . . . . . . {intSmsParts} page{(intSmsParts > 1 ? "s" : null)}";
+        }
+
+        public static int GetMessageParts(int length)
+        {
+            if (length <= 160) return 1;
+
+            return (int)Math.Ceiling(length / 153.0);
+        }
+
+        public static int GetValidGsmTextLength(this string smsText)
+        {
+            smsText = smsText.Replace("\r", "");
+
+            var strGSMTable = "";
+            strGSMTable += "@£$¥èéùìòÇØøÅåΔ_ΦΓΛΩΠΨΣΘΞ`ÆæßÉ !\"#¤%&'()*=,-./0123456789:;<=>?¡";
+            strGSMTable += "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ`¿abcdefghijklmnopqrstuvwxyzäöñüà\r\n";
+
+            var strExtendedTable = "^{}\\[~]|€";
+            var cntGSMOutput = 0;
+
+            for (var i = 0; i < smsText.Length; i++)
+            {
+                var cPlainText = smsText[i];
+
+                var intGSMTable = strGSMTable.IndexOf(cPlainText);
+                if (intGSMTable != -1)
+                {
+                    cntGSMOutput += 1;
+                    continue;
+                }
+                var intExtendedTable = strExtendedTable.IndexOf(cPlainText);
+                if (intExtendedTable != -1)
+                {
+                    cntGSMOutput += 2;
+                }
+                else
+                {
+                    cntGSMOutput += 8;
+                }
+            }
+            return cntGSMOutput;
+        }
+
+        public static List<string> ConvertRawUploadToList(this string rawString, int countryCode)
+        {
+            var stream = new MemoryStream(Encoding.ASCII.GetBytes(rawString));
+            return stream.ConvertRawUploadToList(countryCode);
+        }
+
+        public static List<string> ConvertRawUploadToList(this Stream rawStream, int countryCode)
+        {
+            int p = 0;
+            var sr = new StreamReader(rawStream);
+            var currNumber = new List<char>();
+            var bulkNumbers = new List<string>();
+            var countryCodeArr = countryCode.ToString().ToCharArray();
+
+            while (!(p < 0))
+            {
+                p = sr.Read();
+                //U.InputStream.ReadByte
+                //if char code is numeric
+                if (p >= 48 & p <= 57)
+                {
+                    currNumber.Add(Convert.ToChar(p));
+                }
+                else
+                {
+                    if (currNumber.Count > 5)
+                    {
+                        if (currNumber[0] == '0')
+                        {
+                            currNumber.RemoveRange(0, 1);
+                            currNumber.InsertRange(0, countryCodeArr);
+                        }
+                        bulkNumbers.Add(string.Concat(currNumber.ToArray()));
+                    }
+                    if (currNumber.Count > 0)
+                        currNumber.Clear();
+                }
+            }
+            //take care of the vary last number
+            if (currNumber.Count > 5)
+            {
+                if (currNumber[0] == '0')
+                {
+                    currNumber.RemoveRange(0, 1);
+                    currNumber.InsertRange(0, countryCodeArr);
+                }
+                bulkNumbers.Add(string.Concat(currNumber.ToArray()));
+            }
+            //=================================================
+            //ohowojeheri ruby
+
+            return bulkNumbers.Distinct().ToList();
+        }
+
+    }
+}
---
+++ SMSLive247.Blazor2/ViewModels/ContactModel.cs
@@ -0,0 +1,35 @@
+﻿using SMSLive247.OpenApi;
+
+namespace SMSLive247.Blazor2.Pages.ViewModels
+{
+    public record class ContactModel
+    {
+        public string Key { get; init; }
+        public string Name { get; init; }
+        public int Count { get; init; }
+        public bool Selected { get; set; } = false;
+        public bool Visible { get; set; } = true;
+
+        public ContactModel() { }
+
+        public ContactModel(string num)
+            : this(num, num, 1, null, true) { }
+
+        //public ContactModel(GroupResponse group)
+        //    : this(group.GroupName, group.GroupName, group.Members.Count) { }
+
+        public ContactModel(ContactResponse contact) 
+            : this(contact.PhoneNumber, contact.ContactName, 1, contact) { }
+        public ContactModel(BatchFileResponse batchFile) 
+            : this(batchFile.BatchFileID, batchFile.Description, batchFile.TotalNumbers, null) { }
+
+        private ContactModel(string key, string name, int count, ContactResponse? contact, bool selected = false)
+        {
+            Key = key;
+            Name = name;
+            Count = count;
+            Selected = selected;
+            //Response = contact ?? new() { ContactName = name, PhoneNumber = key };
+        }
+    }
+}
