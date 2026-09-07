c04cf29da40e504878bf6056d132043cfc0a6efd
Ajala Ayodeji <dejisys@idevworks.com>
2026-09-07 18:06:01 +0100

    Fixes

---
+++ SMSLive247.Blazor2/Shared/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+using SMSLive247.OpenApi;
+
+namespace SMSLive247.Blazor2
+{
+    public static class Extensions
+    {
+        public static async ValueTask DownloadFromStream(this IJSRuntime js, BatchFileResponse file, ApiClient apiClient)
+        {
+            var response = await apiClient.BatchFileDownloadAsync(file.BatchFileID);
+            var safeName = string.IsNullOrEmpty(file.Description) ? file.BatchFileID : file.Description;
+            var fileName = $"{safeName}-downloaded-{DateTime.Now:dd-MMM-yyyy}.{file.FileType.ToLower()}";
+
+            await js.DownloadFromStream(fileName, response.Stream);
+        }
+
+        public static async ValueTask DownloadFromStream(this IJSRuntime js, string fileName, Stream stream)
+        {
+            using var streamRef = new DotNetStreamReference(stream, false);
+            await js.InvokeVoidAsync("downloadFileFromStream", fileName, streamRef);
+        }
+    }
+}
